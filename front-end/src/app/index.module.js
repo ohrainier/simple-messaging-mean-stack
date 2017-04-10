a1f2b891d61a368607b79fedf02af3ad3dd49887
Osiana <OSIANAHV@legal.regn.net>
2017-04-11 01:21:08 +0800

    Added registration layout and controller

--- front-end/src/app/index.module.js
+++ front-end/src/app/index.module.js
@@ -4,6 +4,7 @@ import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
+import { AuthController } from './auth/auth.controller';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
@@ -18,5 +19,6 @@ angular.module('myMessageFront', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSaniti
   .service('githubContributor', GithubContributorService)
   .service('webDevTec', WebDevTecService)
   .controller('MainController', MainController)
+  .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective);
