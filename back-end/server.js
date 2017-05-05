4a936a85dc72b36d10babc13c68a39c1c28d6ea5
Osiana <ohrainier@gmail.com>
2017-05-06 00:56:16 +0800

    Added mongo connection setup and web service messages

--- back-end/server.js
+++ back-end/server.js
@@ -3,50 +3,37 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.Promise = require('bluebird');
-
-var Message = mongoose.model('Message',{
-    msg: String
-})
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
 app.use(bodyParser.json());
 
-app.use(function(req,res, next){
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 })
 
-app.get('/api/message', GetMessages)
+app.get('/api/message', message.get);
 
-app.post('/api/message', function(req, res){
-    console.log(req.body);
+app.post('/api/message', message.post);
 
-    var message = new Message(req.body);
+app.post('/auth/register', auth.register);
 
-    message.save();
+//function GetMessages(req, res){
+//    Message.find({}).exec(function(err, result){
+//        res.send(result);
+//    })
+//
+//}
 
-    res.status(200);
-})
-
-app.post('/auth/register', function(req, res){
-    console.log(req.body);
-})
-
-function GetMessages(req, res){
-    Message.find({}).exec(function(err, result){
-        res.send(result);
-    })
-
-}
-
-mongoose.connect("mongodb://localhost:27017/test", function(err, db){
-    if(!err){
+mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
+    if (!err) {
         console.log("we are connected to mongo");
-
     }
 })
 
-var server = app.listen(5000, function (){
+
+var server = app.listen(5000, function () {
     console.log('listening on port', server.address().port)
 })
