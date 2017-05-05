696c9a5df262223e033a2600867c44938c5570fa
Osiana <ohrainier@gmail.com>
2017-05-06 00:56:47 +0800

    Added user authentication and tokenizer

--- front-end/src/app/auth/auth.controller.js
+++ front-end/src/app/auth/auth.controller.js
@@ -1,12 +1,34 @@
 export class AuthController {
 
-  constructor($auth){
-      'ngInject';
+    constructor($auth) {
+        'ngInject';
 
-      this.$auth = $auth;
-  }
+        this.$auth = $auth;
+    }
 
-  register(){
-      this.$auth.signup(this.user);
-  }
+    register() {
+        var vm = this;
+
+        this.$auth.signup(this.user).then(function (token) {
+            vm.$auth.setToken(token);
+        });
+    }
 }
+
+//    register() {
+//        var vm = this;
+//
+//        this.$auth.signup(this.user).then(function (token) {
+//                vm.$auth.setToken(token);
+//            }
+//        )
+//
+//        login() {
+//            var vm = this;
+//
+//            this.$auth.login(this.login.user).then(function (token) {
+//                    vm.$auth.setToken(token);
+//                }
+//            )
+//        }
+//    }
